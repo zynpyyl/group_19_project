be1e16644eade530de30d2454e9e7de30a327fd6
JALE <jalemedlin@gmail.com>
2022-04-28 16:09:53 +0300

    Step 3 is added and work is done

--- Jale_Test02.java
+++ Jale_Test02.java
@@ -2,5 +2,6 @@ public class Jale_Test02 {
     public static void main(String[] args) {
         // Step 1 is done
         // step 2 is done
+        // Driver.getDriver()get(ConfigurationReader.getProperty(("url")));
     }
 }
